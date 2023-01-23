4b59789b07e454f44fa4c7568b379bf793340b30
Naimur Hasan <37500414+naimurhasan@users.noreply.github.com>
2023-01-23 19:31:43 +0600

    clean

--- _code.cpp
+++ _code.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-
+#include<stdio.h>
+#include<conio.h>
 int main(){
-    
+    int roll;
+    scanf("%d", &roll);
+    if(roll==1)
+        printf("You are first.");
+    else
+        printf("You are not first.");
+    getch();
     return 0;
 }
